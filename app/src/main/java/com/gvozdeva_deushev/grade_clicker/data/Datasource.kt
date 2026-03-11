40266525ab55b71255d3ee1054062bac55c1d478
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-03-11 14:46:38 +0300

    Step 5: Basic model and file

---
+++ app/src/main/java/com/gvozdeva_deushev/grade_clicker/data/Datasource.kt
@@ -0,0 +1,14 @@
+package com.gvozdeva_deushev.grade_clicker.data
+
+import com.gvozdeva_deushev.grade_clicker.R
+import com.gvozdeva_deushev.grade_clicker.model.Grade
+
+object Datasource {
+    val gardeList = listOf(
+        Grade(R.drawable.grade_0, 5, 0),
+        Grade(R.drawable.grade_50, 5, 50),
+        Grade(R.drawable.grade_70, 5, 70),
+        Grade(R.drawable.grade_90, 5, 90),
+        Grade(R.drawable.grade_100, 5, 100),
+    )
+}
